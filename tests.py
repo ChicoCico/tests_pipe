04cf23778754d2f564a2d083f5150b4a03f0c5b8
ChicoCico <159574521+ChicoCico@users.noreply.github.com>
2024-02-10 12:16:09 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,11 @@
+import pytest  
+import simple_calc   
+
+def test_add():  
+    assert simple_calc.add(2, 2) == 4 
+def test_devide():
+    assert simple_calc.divide(8, 2) == 4 
+def test_multiply():
+    assert simple_calc.multiply(2, 3) == 6 
+def test_subtract():
+    assert simple_calc.subtract(2, 0) == 1
